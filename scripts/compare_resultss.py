b23ffead5f0a408fa6d7d5ed560eb8dbf8d280a1
Fabian <faabton@gmail.com>
2024-10-29 10:38:49 +0100

    committor vs PB: results for different noise levels

--- scripts/compare_resultss.py
+++ scripts/compare_resultss.py
@@ -11,6 +11,7 @@ class committor():
         self.x = np.sort(np.unique(self.data[:, 0]))
         self.dx = self.x[1] - self.x[0]
         self.y = np.sort(np.unique(self.data[:, 1]))
+        self.resolution = [len(self.x), len(self.y)]
         self.dy = self.y[1] - self.y[0]
         Z = np.where(self.data[:,2] == 0, 1e-22, self.data[:,2]) # Avoid log(0)
         self.Z = Z.reshape((len(self.y), len(self.x)))
@@ -48,8 +49,9 @@ class committor():
         ax.plot(path[:, 0], path[:, 1], color='blue', label='Gradient ascent', linewidth=2)
         return fig, ax
 
-    def plot_gradient(self, fig, ax):
-        ax.quiver(self.X, self.Y, self.dZdx, self.dZdy, color='red', label='Gradient field')
+    def plot_gradient(self, fig, ax, stepx=1, stepy=1 ):
+        #ax.quiver(self.X, self.Y, self.dZdx, self.dZdy, color='red', label='Gradient field')
+        ax.quiver(self.X[::stepy, ::stepx], self.Y[::stepy, ::stepx], self.dZdx[::stepy, ::stepx], self.dZdy[::stepy, ::stepx], color='red', label='Gradient field')
         return fig, ax
     
     def plot_contour(self, fig, ax, lognorm = False, levels=None):
@@ -59,7 +61,7 @@ class committor():
         if levels is None:
             levels = 100
         t=ax.contourf(
-            self.X, self.Y, self.Z, levels=levels, alpha=0.8, cmap='viridis', norm=norm
+            self.X, self.Y, self.Z, levels=levels, alpha=1.0, cmap='viridis', norm=norm
             )
         ax.set_xlabel(r"$t_{init}$")
         ax.set_ylabel(r"$x_{init}$")
@@ -89,20 +91,20 @@ if __name__ == "__main__":
     from DoubleWell_Model import DoubleWell_1D
     model = DoubleWell_1D(mu = 0.03)
 
-    file_committor = "../temp/simulation_grid320_noise_5e2.txt"
+    file_committor = "../temp/simulation_grid6040_noise_5e2.txt"
     comm = committor(file_committor)
-    comm.set_gradients()
-    path = comm.gradient_ascent(0.5, -1.0, lr=0.01)
+    comm.set_gradients(method='nearest')
+    #path = comm.gradient_ascent(1.5, -0.985, lr=0.01)
 
-    fig, ax = plt.subplots(1, 1, figsize=(6, 6), dpi=250)
-    fig, ax = comm.plot_contour(fig, ax)
-    #fig, ax = comm.plot_gradient(fig, ax)
+    fig, ax = plt.subplots(figsize=(10, 7), dpi=250)
+    fig, ax = comm.plot_contour(fig, ax, levels=40)
+    #fig, ax = comm.plot_gradient(fig, ax, stepx=2, stepy=5)
     #fig, ax = comm.plot_ascent_path(fig, ax, path)
     fig, ax = plot_PB(fig, ax, model)
-    ax.set_title(r"Medium noise: $g=0.05$")
+    ax.set_title(r"Medium noise: $g=0.05$; "+f' Res: {comm.resolution}')
     ax.legend()
     fig.savefig("../temp/noise_5e2.png")
-    print('Saved to ../temp/noise_5e2.png')
+    print('Figure saved')
 
 
 
